016b7c6cdea01b1da63e2a9fd385678fbab6a186
Gennady Vedernikov <revoltlight@icloud.com>
2023-03-03 09:56:36 +0300

    End project

---
+++ FathersKickMarathonV1/GradientView.swift
@@ -0,0 +1,95 @@
+//
+//  GradientView.swift
+//  FathersKickMarathonV1
+//
+//  Created by Геннадий Ведерников on 03.03.2023.
+//
+
+import Foundation
+import UIKit
+
+class GradientView: UIView {
+    
+    enum Point {
+        case topLeading
+        case leading
+        case bottomLeading
+        case top
+        case center
+        case bottom
+        case topTrailing
+        case trailing
+        case bottomTrailing
+
+        var point: CGPoint {
+            switch self {
+            case .topLeading:
+                return CGPoint(x: 0, y: 0)
+            case .leading:
+                return CGPoint(x: 0, y: 0.5)
+            case .bottomLeading:
+                return CGPoint(x: 0, y: 1.0)
+            case .top:
+                return CGPoint(x: 0.5, y: 0)
+            case .center:
+                return CGPoint(x: 0.5, y: 0.5)
+            case .bottom:
+                return CGPoint(x: 0.5, y: 1.0)
+            case .topTrailing:
+                return CGPoint(x: 1.0, y: 0.0)
+            case .trailing:
+                return CGPoint(x: 1.0, y: 0.5)
+            case .bottomTrailing:
+                return CGPoint(x: 1.0, y: 1.0)
+            }
+        }
+    }
+    
+    @IBInspectable private var startColor: UIColor? {
+        didSet {
+            setupGradientColors()
+        }
+    }
+    
+    @IBInspectable private var endColor: UIColor? {
+        didSet {
+            setupGradientColors()
+        }
+    }
+    
+    private let gradientLayer = CAGradientLayer()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupGradient()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer.frame = bounds
+        gradientLayer.cornerRadius = 15
+        
+        gradientLayer.shadowColor = UIColor.black.cgColor
+        gradientLayer.shadowOpacity = 1
+        gradientLayer.shadowOffset = .zero
+        gradientLayer.shadowRadius = 15
+    }
+    
+    private func setupGradient() {
+        self.layer.addSublayer(gradientLayer)
+        setupGradientColors()
+        gradientLayer.startPoint = Point.leading.point
+        gradientLayer.endPoint = Point.trailing.point
+        
+    }
+    
+    private func setupGradientColors() {
+        if let startColor = startColor, let endColor = endColor {
+            gradientLayer.colors = [startColor.cgColor, endColor.cgColor]
+        }
+    }
+}
